a96951b3a160a657686fbf0c1d2a76e9fc98b0f8
Ruben3182 <sarkisyan3182@gmail.com>
2024-12-11 13:07:28 -0500

    aap-developing

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -26,6 +26,7 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
@@ -48,7 +49,7 @@ const CartItem = ({ onContinueShopping }) => {
                 <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
               </div>
               <div className="cart-item-total">Total: ${calculateTotalCost(item)}</div>
-              <button className="cart-item-delete" onClick={() => handleRemove(item)}>Delete</button>
+              <button className="cart-item-delete" onClick={() => handleRemove(item.name)}>Delete</button>
             </div>
           </div>
         ))}
